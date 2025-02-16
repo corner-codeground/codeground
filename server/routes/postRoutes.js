ed1f9b30803670c86f275299a3b043d4cb51f566
getpoorpoo <getpoorpoo@gmail.com>
2025-02-16 16:31:18 +0900

    Fix: 잘못 들어간 코드 삭제

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
-const trendingPostController = require("../controllers/trendingPostController");
 const { isLoggedIn } = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware");
 
